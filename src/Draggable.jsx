a22de9e3b2615d642dbfe7ccb19ede0d191d100d
Izzatillo Azimov <croosdev@gmail.com>
2023-06-02 20:13:00 +0500

    draggable fixed

--- src/Draggable.jsx
+++ src/Draggable.jsx
@@ -1,5 +1,19 @@
 import { useMemo, useRef, useState } from 'react';
 import { Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
 const DraggableMarker = ({ setLat, setLng, draggable }) => {
   const [center] = useState({ lat: 41.309805, lng: 69.248903 });
   const markerRef = useRef(null);
